7f354c38d83cfb77a89ac9fac143779c7c3efbe5
kracheff <kracheff@mail.bg>
2016-04-18 12:03:51 +0300

    Adding first touch of the Box class

---
+++ src/engine/Box.java
@@ -0,0 +1,14 @@
+package engine;
+
+/**
+ * Created by user on 18.04.2016.
+ */
+public class Box {
+    public int x;
+    public int y;
+
+    public Box (int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
