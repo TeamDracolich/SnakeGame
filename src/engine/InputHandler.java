27788635f2396e6809bb1a44e2886af761757f9d
JivkoDev <zhkanev_mf@yahoo.com>
2016-04-19 09:34:15 +0100

    Added files via upload

---
+++ src/engine/InputHandler.java
@@ -0,0 +1,56 @@
+/**
+ * Created by Jivko on 19-Apr-16.
+ */
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class InputHandler implements KeyListener{
+
+    public InputHandler(Game game){
+        game.addKeyListener(this);
+    }
+
+
+    public void keyPressed(KeyEvent e) {
+        int keyCode = e.getKeyCode();
+
+        if (keyCode == KeyEvent.VK_W || keyCode == KeyEvent.VK_UP) {
+            if(Game.snake.getVelY() != 20){
+                Game.snake.setVelX(0);
+                Game.snake.setVelY(-20);
+            }
+        }
+        if (keyCode == KeyEvent.VK_S || keyCode == KeyEvent.VK_DOWN) {
+            if(Game.snake.getVelY() != -20){
+                Game.snake.setVelX(0);
+                Game.snake.setVelY(20);
+            }
+        }
+        if (keyCode == KeyEvent.VK_D || keyCode == KeyEvent.VK_RIGHT) {
+            if(Game.snake.getVelX() != -20){
+                Game.snake.setVelX(20);
+                Game.snake.setVelY(0);
+            }
+        }
+        if (keyCode == KeyEvent.VK_A || keyCode == KeyEvent.VK_LEFT) {
+            if(Game.snake.getVelX() != 20){
+                Game.snake.setVelX(-20);
+                Game.snake.setVelY(0);
+            }
+        }
+        //Other controls
+        if (keyCode == KeyEvent.VK_ESCAPE) {
+            System.exit(0);
+        }
+    }
+
+
+    public void keyReleased(KeyEvent e) {
+    }
+
+
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+}
